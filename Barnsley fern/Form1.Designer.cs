21003803cffe6019a5191b7c4cad877079e31de3
Kasha_ne_Parasha <283908@niuitmo.ru>
2020-05-27 19:14:55 +0300

    first

---
+++ Barnsley fern/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿using System.Windows.Forms;
+
+namespace Barnsley_fern
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._PictureBox = new System.Windows.Forms.PictureBox();
+            this.Fern = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this._PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _PictureBox
+            // 
+            this._PictureBox.Location = new System.Drawing.Point(-17, -1);
+            this._PictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this._PictureBox.Name = "_PictureBox";
+            this._PictureBox.Size = new System.Drawing.Size(1448, 788);
+            this._PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this._PictureBox.TabIndex = 0;
+            this._PictureBox.TabStop = false;
+            this._PictureBox.Click += new System.EventHandler(this.FernPictureBox_Click);
+            // 
+            // Fern
+            // 
+            this.Fern.AccessibleDescription = "";
+            this.Fern.AccessibleName = "";
+            this.Fern.Location = new System.Drawing.Point(12, -1);
+            this.Fern.Name = "Fern";
+            this.Fern.Size = new System.Drawing.Size(115, 32);
+            this.Fern.TabIndex = 1;
+            this.Fern.Text = "Fern\r\n";
+            this.Fern.UseVisualStyleBackColor = true;
+            this.Fern.Click += new System.EventHandler(this.button_fern);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Snowflake";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_snow);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear_map";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_clear);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "tree";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_tree);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1460, 786);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Fern);
+            this.Controls.Add(this._PictureBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Barnsley Fern";
+            ((System.ComponentModel.ISupportInitialize)(this._PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox _PictureBox;
+        private Button Fern;
+        private Timer timer1;
+        private Button button2;
+        private Timer timer2;
+        private Button button3;
+        private Button button1;
+        private Timer timer3;
+    }
+}
+
---
+++ Barnsley fern/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Barnsley_fern
+{
+    public partial class Form1 : Form
+    {
+        private const float MinX = -6;
+        private const float MaxX = 6;
+        private const float MinY = 0.1f;
+        private const float MaxY = 10;
+
+        private int PointNumber = 1;
+
+        private int _width;
+        private int _height;
+
+        private Bitmap _bitmap;
+
+        private Bitmap Clear_bitmap;
+
+        private Graphics _graph;
+
+        private Graphics _clear_graph;
+
+       
+
+        private float[] fern_probability = new float[4]
+        {
+            0.01f,
+            0.2f,
+            0.2f,
+            0.85f
+        };
+
+        private float[] snow_probability = new float[4]
+        {
+            0.1f,
+            0.2f,
+            0.3f,
+            0.45f
+        };
+
+        private float[] tree_probability = new float[5]
+        {
+            0.07f,
+            0.5f,
+            0.3f,
+            0.2f,
+            0.4f
+        };
+
+        private float[,] Barn_fern = new float[4, 6]
+        {
+            {0,      0,      0,     0.16f, 0, 0    },
+            {-0.15f, 0.28f,  0.26f, 0.24f, 0, 0.44f},
+            {0.2f,  -0.26f,  0.23f, 0.22f, 0, 1.6f },
+            {0.85f,  0.04f, -0.04f, 0.85f, 0, 1.6f }
+        };
+
+        private float[,] snowflake = new float[3, 6]
+        {
+            { 0.5f, 0.0f, 0.0f, -0.5f, 0.0f, 1.0f },
+            { 0.0f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f },
+            { 0.5f, 0.0f, 0.0f, 0.5f, 0.5f, 0.5f}
+        };
+
+        private float[,] tree = new float[5, 6]
+        {
+            { 0.1950f, -0.4880f, 0.344f, 0.443f, 0.4431f, 0.2452f },
+            { 0.4620f, 0.4140f, -0.2520f, 0.3610f, 0.2511f, 0.5692f },
+            { -0.0580f, -0.0700f, 0.4530f, -0.1110f, 0.5976f, 0.0969f },
+            { -0.0350f, -0.0700f, -0.4690f, 0.0220f, 0.4884f, 0.5069f },
+            { -0.6370f, 0.0f, 0.0f, 0.5010f, 0.8562f, 0.2513f },
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Clear_bitmap = new Bitmap(_PictureBox.Width, _PictureBox.Height);
+            _clear_graph = Graphics.FromImage(Clear_bitmap);
+            _clear_graph.Clear(Color.Black);
+        }
+
+        private void DrawSnow()
+        {
+            Random rnd = new Random();
+
+            float xtemp = 0, ytemp = 0;
+
+            int func_num = 0;
+
+            for (int i = 1; i <= PointNumber; i++)
+            {
+                var num = rnd.NextDouble();
+
+                for (int j = 0; j <= 2; j++)
+                {
+                    num = num - snow_probability[j];
+
+                    if (num <= 0)
+                    {
+                        func_num = j;
+                        break;
+                    }
+                }
+
+                var x = snowflake[func_num, 0] * xtemp + snowflake[func_num, 1] * ytemp + snowflake[func_num, 4];
+                var y = snowflake[func_num, 2] * xtemp + snowflake[func_num, 3] * ytemp + snowflake[func_num, 5];
+
+                xtemp = x;
+                ytemp = y;
+
+                x = (int)(xtemp * _width + _PictureBox.Width / 2);
+                y = (int)(ytemp * _height);
+
+                _bitmap.SetPixel((int)x, (int)y, Color.BlueViolet);
+
+            }
+            
+            _PictureBox.BackgroundImage = _bitmap;
+        }
+
+        private void DrawTree()
+        {
+            Random rnd = new Random();
+
+            float xtemp = 0, ytemp = 0;
+
+            int func_num = 0;
+
+            for (int i = 1; i <= PointNumber; i++)
+            {
+                var num = rnd.NextDouble();
+
+                for (int j = 0; j <= 4; j++)
+                {
+                    num = num - tree_probability[j];
+
+                    if (num <= 0)
+                    {
+                        func_num = j;
+                        break;
+                    }
+                }
+
+                var x = tree[func_num, 0] * xtemp + tree[func_num, 1] * ytemp + tree[func_num, 4];
+                var y = tree[func_num, 2] * xtemp + tree[func_num, 3] * ytemp + tree[func_num, 5];
+
+                xtemp = x;
+                ytemp = y;
+
+                x = (int)(xtemp * _width + _PictureBox.Width / 2);
+                y = (int)(ytemp * _height);
+
+                _bitmap.SetPixel((int)x, (int)y, Color.Green);
+
+            }
+            _bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
+            _PictureBox.BackgroundImage = _bitmap;
+        }
+
+        private void DrawFern()
+        {
+            Random rnd = new Random();
+            
+            float xtemp = 0, ytemp = 0;
+            
+            int func_num = 0;
+
+            for (int i = 1; i <= PointNumber; i++)
+            {
+                var num = rnd.NextDouble();
+                
+                for (int j = 0; j <= 3; j++)
+                {
+                    num = num - fern_probability[j];
+
+                    if (num <= 0)
+                    {
+                        func_num = j;
+                        break;
+                    }
+                }
+
+                var x = Barn_fern[func_num, 0] * xtemp + Barn_fern[func_num, 1] * ytemp + Barn_fern[func_num, 4];
+                var y = Barn_fern[func_num, 2] * xtemp + Barn_fern[func_num, 3] * ytemp + Barn_fern[func_num, 5];
+
+                xtemp = x;
+                ytemp = y;
+
+                x = (int)(xtemp * _width + _PictureBox.Width / 2);
+                y = (int)(ytemp * _height);
+
+                _bitmap.SetPixel((int)x, (int)y, Color.LawnGreen);
+
+            }
+            _bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
+            _PictureBox.BackgroundImage = _bitmap;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _PictureBox.BackgroundImage = Clear_bitmap;
+
+            _bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
+
+            PointNumber += 100;
+
+            DrawFern();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            _PictureBox.BackgroundImage = Clear_bitmap;
+
+            PointNumber += 100;
+
+            DrawSnow();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            _PictureBox.BackgroundImage = Clear_bitmap;
+
+            _bitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
+
+            PointNumber += 100;
+
+            DrawTree();
+        }
+
+        private void button_snow(object sender, EventArgs e)
+        {
+            _width = (int)((_PictureBox.Width * 6) / (MaxX - MinX));
+            _height = (int)((_PictureBox.Height * 6) / (MaxY - MinY));
+            _bitmap = new Bitmap(_PictureBox.Width * 6, _PictureBox.Height * 6);
+            _graph = Graphics.FromImage(_bitmap);
+            _graph.Clear(Color.Black);
+
+            timer2.Enabled = true;
+        }
+
+        private void button_fern(object sender, EventArgs e)
+        {
+            _width = (int)((_PictureBox.Width / 1.5) / (MaxX - MinX));
+            _height = (int)((_PictureBox.Height / 1.5) / (MaxY - MinY));
+            _bitmap = new Bitmap(_PictureBox.Width, _PictureBox.Height);
+            _graph = Graphics.FromImage(_bitmap);
+            _graph.Clear(Color.Black);
+
+            timer1.Enabled = true;
+        }
+
+        private void button_tree(object sender, EventArgs e)
+        {
+            _width = (int)((_PictureBox.Width * 6) / (MaxX - MinX));
+            _height = (int)((_PictureBox.Height * 6) / (MaxY - MinY));
+            _bitmap = new Bitmap(_PictureBox.Width, _PictureBox.Height);
+            _graph = Graphics.FromImage(_bitmap);
+            _graph.Clear(Color.Black);
+
+            timer3.Enabled = true;
+        }
+
+        private void button_clear(object sender, EventArgs e)
+        {
+            _PictureBox.BackgroundImage = Clear_bitmap;
+
+            timer1.Enabled = false;
+
+            timer2.Enabled = false;
+
+            timer3.Enabled = false;
+        }
+
+        private void FernPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
