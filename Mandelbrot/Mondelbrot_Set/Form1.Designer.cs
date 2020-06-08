8c3d787f905fdc44622606aff4a9dcf38151ff26
Kasha_ne_Parasha <283908@niuitmo.ru>
2020-06-08 13:24:42 +0300

    sec

---
+++ Mandelbrot/Mondelbrot_Set/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Mondelbrot_Set
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Drow_Mandelbrot = new System.Windows.Forms.Button();
+            this.imPict = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Auto_Scale = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imPict)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Drow_Mandelbrot
+            // 
+            this.Drow_Mandelbrot.Location = new System.Drawing.Point(819, 12);
+            this.Drow_Mandelbrot.Name = "Drow_Mandelbrot";
+            this.Drow_Mandelbrot.Size = new System.Drawing.Size(139, 68);
+            this.Drow_Mandelbrot.TabIndex = 0;
+            this.Drow_Mandelbrot.Text = "Drow_Mandelbrot";
+            this.Drow_Mandelbrot.UseVisualStyleBackColor = true;
+            this.Drow_Mandelbrot.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imPict
+            // 
+            this.imPict.Location = new System.Drawing.Point(12, 0);
+            this.imPict.Name = "imPict";
+            this.imPict.Size = new System.Drawing.Size(801, 574);
+            this.imPict.TabIndex = 1;
+            this.imPict.TabStop = false;
+            this.imPict.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.imPict.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Auto_Scale
+            // 
+            this.Auto_Scale.Location = new System.Drawing.Point(819, 86);
+            this.Auto_Scale.Name = "Auto_Scale";
+            this.Auto_Scale.Size = new System.Drawing.Size(139, 61);
+            this.Auto_Scale.TabIndex = 2;
+            this.Auto_Scale.Text = "Auto scale";
+            this.Auto_Scale.UseVisualStyleBackColor = true;
+            this.Auto_Scale.Click += new System.EventHandler(this.Auto_Scale_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 576);
+            this.Controls.Add(this.Auto_Scale);
+            this.Controls.Add(this.imPict);
+            this.Controls.Add(this.Drow_Mandelbrot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imPict)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Drow_Mandelbrot;
+        private System.Windows.Forms.PictureBox imPict;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Auto_Scale;
+    }
+}
+
---
+++ Mandelbrot/Mondelbrot_Set/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mondelbrot_Set
+{
+    public partial class Form1 : Form
+    {
+        double x_min = -1.5, y_min = -1.5, x_max = 1.5, y_max = 1.5;
+        double hx, hy, x_, y_, n = 0;
+
+        Bitmap newBitmap;
+
+        Size size;
+        double SizeArea, ScaleArea;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            newBitmap = new Bitmap(imPict.Width, imPict.Height);
+            imPict.Image = newBitmap;
+            size = imPict.Size;
+
+            SizeArea = 3;
+            ScaleArea = 3;
+        }
+
+        private Color MandelBrot(double a, double b)
+        {
+            double x = 0, y = 0, xy, x2, y2, r = 0;
+            int k = 100, loopgo = 0;
+
+            do
+            {
+                loopgo++;
+                x2 = Math.Pow(x, 2);
+                y2 = Math.Pow(y, 2);
+                xy = x * y;
+                x = x2 - y2 + a;
+                y = 2 * xy + b;
+                r = x2 + y2;
+            } 
+            while (k > loopgo && r < 4);
+
+            if (loopgo == k)
+                return Color.FromArgb(k, k, k);
+            else
+                return Color.FromArgb(loopgo % 2 * 50, loopgo % 4 * 30, loopgo % 2 * 100);
+        }
+
+        private void Draw()
+        {
+            newBitmap = new Bitmap(imPict.Width, imPict.Height);
+            imPict.Image = newBitmap;
+            size = imPict.Size;
+
+            Color tcolor = Color.Purple;
+
+            for (int i = 0; i < imPict.Width; i++)
+            {
+                x_ = (hx - SizeArea / 2) + i * (SizeArea / size.Width);
+
+                for (int j = 0; j < imPict.Height; j++)
+                { 
+                    y_ = (hy - SizeArea / 2) + j * (SizeArea / size.Height);
+
+                    tcolor = MandelBrot(x_, y_);
+                        
+                    newBitmap.SetPixel(i, j, tcolor);
+                }
+            }
+
+            imPict.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void Auto_Scale_Click(object sender, EventArgs e)
+        {
+            x_min = -1.5; y_min = -1; x_max = 1; y_max = 1;
+            timer1.Enabled = true;
+            Draw();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int CursorX = Cursor.Position.X;
+            int CursorY = Cursor.Position.Y;
+
+            int X = CursorX, Y = CursorY;
+
+            hx = (hx - SizeArea / 2) + X * (SizeArea / size.Width);
+            hy = (hy - SizeArea / 2) + Y * (SizeArea / size.Height);
+            SizeArea /= ScaleArea;
+            Draw();
+        }
+
+        private void pictureBox1_Click(object sender, MouseEventArgs e)
+        {
+            int X = e.X,
+                Y = e.Y;
+
+            string _x = X.ToString();
+            string _y = Y.ToString();
+
+            Console.WriteLine($"{_x}, {_y}");
+            
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    hx = (hx - SizeArea / 2) + X * (SizeArea / size.Width);
+                    hy = (hy - SizeArea / 2) + Y * (SizeArea / size.Height);
+                    SizeArea /= ScaleArea;
+                    Draw();
+                    break;
+                case MouseButtons.Middle:
+                    x_max = 1; y_max = 1;
+                    SizeArea = 3;
+                    ScaleArea = 3;
+                    Draw();
+                    break;
+                case MouseButtons.Right:
+                    x_ = (hx - SizeArea / 2) + X * (SizeArea / size.Width);
+                    y_ = (hy - SizeArea / 2) + Y * (SizeArea / size.Height);
+                    SizeArea *= ScaleArea;
+                    Draw();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e){}
+    }
+}
